f770ed45b3692ad6842312eca3fb321582356e7c
lingdong <lingdong@bantouyan.com>
2026-03-18 20:52:54 +0800

    fix:log time zone

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::fmt::time::LocalTime;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::EnvFilter;
 
 pub mod agent;
+pub mod im;
 pub mod llm;
 pub mod tool;
 pub mod util;
-pub mod im;
 
 #[tokio::main]
 async fn main() {
@@ -15,10 +16,18 @@ async fn main() {
     let file_appender = tracing_appender::rolling::daily("logs", "arknights.log");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
 
+    let timer = LocalTime::rfc_3339();
+
     tracing_subscriber::registry()
         .with(EnvFilter::new("info"))
         .with(tracing_subscriber::fmt::layer())
-        .with(tracing_subscriber::fmt::layer().with_ansi(false).with_writer(non_blocking))
+        .with(tracing_subscriber::fmt::layer().with_timer(timer.clone()))
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_ansi(false)
+                .with_timer(timer)
+                .with_writer(non_blocking),
+        )
         .init();
 
     // lark wss
